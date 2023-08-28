0ce0d6be3a63b8bd07c53ae029a4c0a942217d1d
Kavibuddy <89762761+Kavi-buddy@users.noreply.github.com>
2023-08-28 11:29:34 +0530

    Add files via upload

---
+++ Calculation.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+
+  int main()
+  {
+          int x,y;
+          int sum;
+          std::cout<<"Type a Number:";
+          //Enter the First Value
+          std::cin>>x;
+          std::cout<<"Type a Another Number:";
+          //Enter the Second Value
+          std::cin>>y;
+          // Add the Two Values
+          sum=x+y;
+          std::cout<<"Sum is:"<<sum;
+          return 0;
+  }
